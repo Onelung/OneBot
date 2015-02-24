d32d8b6a50746adf1a6b9d36c4bd582e60b06624
Onelung <deejay.onelung@gmail.com>
2015-02-24 22:28:19 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -101,7 +101,7 @@
         website: null,
         intervalMessages: [],
         messageInterval: 5,
-        songstats: true,
+        songstats: false,
         commandLiteral: "!",
         blacklists: {
             NSFW: "https://rawgit.com/Onelung/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
